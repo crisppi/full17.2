bd1754cfecc61cd9ce46ab49c9bb1d25be995d7b
crisppi <66572787+crisppi@users.noreply.github.com>
2023-02-03 07:43:33 -0300

    03/02 - 6

--- formularios/form_list_internacao.php
+++ formularios/form_list_internacao.php
@@ -18,7 +18,7 @@
     // $patologiaDao = new patologiaDAO($conn, $BASE_URL);
     // $patologias = $patologiaDao->findGeral();
 
-    $internacao = new internacaoDAO($conn, $BASE_URL);
+    // $internacao = new internacaoDAO($conn, $BASE_URL);
 
     ?>
     <!-- FORMULARIO DE PESQUISAS -->
@@ -174,60 +174,60 @@
                 echo "</div>"; ?>
                 <hr>
             </div>
-            <?php
+            <!-- <?php
 
-            //modo cadastro
-            $formData = "0";
-            $formData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-            $total = $internacao->findTotal();
+                    //modo cadastro
+                    $formData = "0";
+                    $formData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+                    $total = $Internacao->findTotal();
 
-            $totalcasos = $total['0'];
-            // echo $totalcasos['0'];
-            $reg = ($totalcasos['0']);
-            // echo $reg;
+                    $totalcasos = $total['0'];
+                    // echo $totalcasos['0'];
+                    $reg = ($totalcasos['0']);
+                    // echo $reg;
 
-            if ($formData !== "0") {
-                $_SESSION['msg'] = "<p style='color: green;'>Usuário cadastrado com sucesso!</p>";
-                //header("Location: index.php");
-            } else {
-                echo "<p style='color: #f00;'>Erro: Usuário não cadastrado!</p>";
-            };
-
-            try {
-
-                $query_Total = $conn->prepare($sql_Total);
-                $query_Total->execute();
-                $query_result = $query_Total->fetchAll(PDO::FETCH_ASSOC);
-                # conta quantos registros tem no banco de dados
-                $query_count = $query_Total->rowCount();
-
-                # calcula o total de paginas a serem exibidas
-                $qtdIntItens = ceil($reg / $limite);
-            } catch (PDOexception $error_Total) {
-
-                echo 'Erro ao retornar os Dados. ' . $error_Total->getMessage();
-            }
-            echo "<div style=margin-left:20px;>";
-            echo "<div style='color:blue; margin-left:20px;'>";
-            echo "</div>";
-            echo "<nav aria-label='Page navigation example'>";
-            echo " <ul class='pagination'>";
-            echo " <li class='page-item'><a class='page-link' href='list_internacao.php?pg=1&" . $gets . "''><span aria-hidden='true'>&laquo;</span></a></li>";
-            if ($qtdIntItens > 1 && $pg <= $qtdIntItens) {
-                for ($i = 1; $i <= $qtdIntItens; $i++) {
-                    if ($i == $pg) {
-                        echo "<li class='page-item active'><a class='page-link' class='ativo'>" . $i . "</a></li>";
+                    if ($formData !== "0") {
+                        $_SESSION['msg'] = "<p style='color: green;'>Usuário cadastrado com sucesso!</p>";
+                        //header("Location: index.php");
                     } else {
-                        echo "<li class='page-item '><a class='page-link' href='list_internacao.php?pg=$i&" . $gets . "'>" . $i . "</a></li>";
+                        echo "<p style='color: #f00;'>Erro: Usuário não cadastrado!</p>";
+                    };
+
+                    try {
+
+                        $query_Total = $conn->prepare($sql_Total);
+                        $query_Total->execute();
+                        $query_result = $query_Total->fetchAll(PDO::FETCH_ASSOC);
+                        # conta quantos registros tem no banco de dados
+                        $query_count = $query_Total->rowCount();
+
+                        # calcula o total de paginas a serem exibidas
+                        $qtdIntItens = ceil($reg / $limite);
+                    } catch (PDOexception $error_Total) {
+
+                        echo 'Erro ao retornar os Dados. ' . $error_Total->getMessage();
                     }
-                }
-            }
-            echo "<li class='page-item'><a class='page-link' href='list_internacao.php?pg=$qtdIntItens&" . $gets . "''><span aria-hidden='true'>&raquo;</span></a></li>";
-            echo " </ul>";
-            echo "</nav>";
-            echo "</div>"; ?>
+                    echo "<div style=margin-left:20px;>";
+                    echo "<div style='color:blue; margin-left:20px;'>";
+                    echo "</div>";
+                    echo "<nav aria-label='Page navigation example'>";
+                    echo " <ul class='pagination'>";
+                    echo " <li class='page-item'><a class='page-link' href='list_internacao.php?pg=1&" . $gets . "''><span aria-hidden='true'>&laquo;</span></a></li>";
+                    if ($qtdIntItens > 1 && $pg <= $qtdIntItens) {
+                        for ($i = 1; $i <= $qtdIntItens; $i++) {
+                            if ($i == $pg) {
+                                echo "<li class='page-item active'><a class='page-link' class='ativo'>" . $i . "</a></li>";
+                            } else {
+                                echo "<li class='page-item '><a class='page-link' href='list_internacao.php?pg=$i&" . $gets . "'>" . $i . "</a></li>";
+                            }
+                        }
+                    }
+                    echo "<li class='page-item'><a class='page-link' href='list_internacao.php?pg=$qtdIntItens&" . $gets . "''><span aria-hidden='true'>&raquo;</span></a></li>";
+                    echo " </ul>";
+                    echo "</nav>";
+                    echo "</div>"; ?> -->
             <div>
-                <hr>
+
                 <a class="btn btn-success styled" style="margin-left:120px" href="cad_internacao.php">Nova internação</a>
             </div>
         </div>
